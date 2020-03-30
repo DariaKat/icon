7a4b3a7792c2b93c58714959186bdd76dbecd2d1
DariaKat <dari.aria@yandex.ru>
2020-03-30 18:48:23 +0300

    Added icon output and styling

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ function getJSON() {
   request.onload = function () {
     if (request.readyState == 4) {
       if (request.status === 200) {
-        var responseObj = JSON.parse(request.responseText, function (key, value) {
-          let div = document.getElementById("help");
-
-          let p = div.appendChild(document.createElement("p")).innerHTML = key;
-          let img = div.appendChild(document.createElement("img")).src = value;
-
+        let responseObj = JSON.parse(request.responseText, function (key, value) {
+          let section = document.getElementById("grid");
+          let cell = section.appendChild(document.createElement("div"));
+          cell.appendChild(document.createElement("p")).innerHTML = key;
+          cell.appendChild(document.createElement("img")).src = value;
         });
       } else {
         console.error(request.statusText);
