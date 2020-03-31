88f5fe289562e4f7846915317d05a0239e4c8bb1
DariaKat <dari.aria@yandex.ru>
2020-03-31 15:01:15 +0300

    fixed a bug

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function getJSON() {
   var request = new XMLHttpRequest();
-  request.open('get', 'https://api.github.com/emojis', true);
-
-  request.onload = function () {
+  request.open('GET', 'https://api.github.com/emojis', true);
+  request.onload = () => {
     if (request.readyState == 4) {
       if (request.status === 200) {
-        let responseObj = JSON.parse(request.responseText, function (key, value) {
-          let section = document.getElementById("grid");
-          let cell = section.appendChild(document.createElement("div"));
-          cell.appendChild(document.createElement("p")).innerHTML = key;
-          cell.appendChild(document.createElement("img")).src = value;
+        JSON.parse(request.responseText, (key, value) => {
+          if (typeof (key) == 'string' && typeof (value) != 'object') {
+            let section = document.getElementById('grid');
+            let cell = section.appendChild(document.createElement('div'));
+            cell.appendChild(document.createElement('p')).innerHTML = key;
+            cell.appendChild(document.createElement('img')).src = value;
+          }
         });
       } else {
         console.error(request.statusText);
